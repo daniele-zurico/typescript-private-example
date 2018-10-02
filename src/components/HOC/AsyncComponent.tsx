0775d0ad129d9c801e91b6a1c02661f2a1ba98c2
Daniele Zurico <daniele.zurico@blackswan.com>
2018-10-02 16:57:38 +0100

    add a bit of documentation

--- src/components/HOC/AsyncComponent.tsx
+++ src/components/HOC/AsyncComponent.tsx
@@ -1,26 +1,34 @@
+/**
+ * Async component is useful to load whatever component asyncronus
+ * In this way we can do a lazy loading of the component loading the chunk only when required
+ * @example
+ * const asyncLogin = asyncComponent(() => import('./containers/Login/Login'));
+ * The login component will be loaded only when requested and will not be included in the main chunk
+ */
 import * as React from 'react';
 import { Component } from 'react';
 
 interface IState {
-    component: any;
+  component: any;
 }
 
 const asyncComponent = (importComponent: any) => {
-    return class extends Component<IState, {}> {
-        public state = {
-            component: null
-        }
+  return class extends Component<IState, {}> {
+    public state = {
+      component: null,
+    };
 
-        public componentDidMount() {
-            importComponent()
-                .then((cmp: any) => this.setState({ component: cmp.default }));
-        }
-
-        public render() {
-            const C: any = this.state.component;
-            return C ? <C {...this.props} /> : null;
-        }
+    public componentDidMount() {
+      importComponent().then((cmp: any) =>
+        this.setState({ component: cmp.default })
+      );
     }
-}
+
+    public render() {
+      const C: any = this.state.component;
+      return C ? <C {...this.props} /> : null;
+    }
+  };
+};
 
 export default asyncComponent;
--- src/components/HOC/ErrorHandler.tsx
+++ src/components/HOC/ErrorHandler.tsx
@@ -1,12 +1,21 @@
+/**
+ * It will display a modal dialog only when an error
+ * will be passed in the props of the Wrapped component
+ * The main component need to have in the props:
+ * 1) error of type IError
+ * 2) a method called dismissError
+ */
+import { IError } from 'common/interfaces';
 import { Modal } from 'components/UI';
 import * as React from 'react';
 import { Component } from 'react';
 
+interface IProps {
+  error: IError;
+  dismissError: () => void;
+}
+
 const errorHandler = (WrappedComponent: any) => {
-  interface IProps {
-    error: any;
-    dismissError: () => void;
-  }
   return class extends Component<IProps, {}> {
     public dismissHandler = () => {
       this.props.dismissError();

0cea47a1ef2a63cd9cfb40076998a488044fd827
daniele.zurico <daniele.zurico@gohenry.co.uk>
2018-10-05 16:48:51 +0100

    start designing the main dashboard

--- src/containers/Dashboard/Dashboard.tsx
+++ src/containers/Dashboard/Dashboard.tsx
@@ -2,16 +2,72 @@ import { Navigation } from 'components';
 import { INavigationItem } from 'components/Navigation';
 import * as React from 'react';
 import { Component } from "react";
-
+import * as classes from './Dashboard.scss';
 class Dashboard extends Component {
     public items: INavigationItem[] = [
         {
-            href: '#',
-            label: 'Dashboard'
+            href: '/expenses',
+            label: 'Expenses'
+        },
+        {
+            href: '/categories',
+            label: 'Categories'
+        },
+        {
+            href: '/statements',
+            label: 'Statements'
         }
     ]
     public render() {
-        return <Navigation items={this.items} />;
+        return (
+            <div style={{ height: '100vh' }}>
+                <Navigation items={this.items} />
+                <div className={classes.Container}>
+                    <div className={classes.Period}>
+                        Period:
+                        <span className={classes.Date}>27-09-2018</span>
+                        <span> to </span>
+                        <span className={classes.Date}>27-10-2018</span>
+                    </div>
+                    <div className={classes.Card}>
+                        <div className={classes.Title}>Balance</div>
+                        <div className={classes.Body}>
+                            <div className={classes.SpendingCircle}>
+                                <span className={classes.Balance}>100£</span>
+                            </div>
+                            <div className={classes.Details}>
+                                <div><b>Income:</b> 1000 £</div>
+                                <div><b>Expenses:</b> 900 £</div>
+                            </div>
+
+                        </div>
+                        <div className={classes.Footer}>
+                            <div className={classes.FooterLabel}>Details<i className={['fa', 'fa-angle-right'].join(' ')} /></div>
+                        </div>
+                    </div>
+
+                    <div className={classes.CardInfoContainer}>
+                        <div className={[classes.Card, classes.Primary].join(' ')}>
+                            <i className={['fa', 'fa-plus-circle', classes.Fa].join(' ')} />
+                            <div>Add Expenses</div>
+                        </div>
+
+                        <div className={[classes.Card, classes.Accent].join(' ')}>
+                            <i className={['fa', 'fa-plus-circle', classes.Fa].join(' ')} />
+                            <div>Add New Category</div>
+                        </div>
+
+                        <div className={[classes.Card, classes.Info].join(' ')}>
+                            <i className={['fa', 'fa-balance-scale', classes.Fa].join(' ')} />
+                            <div>Statements</div>
+                        </div>
+                    </div>
+
+
+                </div >
+            </div >
+
+        );
     }
 }
 

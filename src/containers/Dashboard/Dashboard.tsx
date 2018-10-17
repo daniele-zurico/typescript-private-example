3a41c72e42968553f2f7483e5e7fb30ef646fa48
Daniele Zurico <daniele.zurico@blackswan.com>
2018-10-17 11:15:16 +0100

    refactor dashboard page

--- src/containers/Dashboard/Dashboard.tsx
+++ src/containers/Dashboard/Dashboard.tsx
@@ -1,65 +1,128 @@
 import { Card, CardType } from 'components';
 import * as React from 'react';
-import { Component } from "react";
+import { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { loadExpensesStart, loadIncomeStart } from 'store/actions';
 import * as classes from './Dashboard.scss';
 /**
  * I just started to design it not valid code
  */
-class Dashboard extends Component {
-    public render() {
-        return (
-            <React.Fragment>
-                <div className={classes.Period}>
-                    Period:
-                        <span className={classes.Date}>27-09-2018</span>
-                    <span> to </span>
-                    <span className={classes.Date}>27-10-2018</span>
-                </div>
-
-                <Card title="Balance" body={
-                    <React.Fragment>
-                        <div className={classes.SpendingCircle}>
-                            <span className={classes.Balance}>100£</span>
-                        </div>
-                        <div className={classes.Details}>
-                            <div><b>Income:</b> 1000 £</div>
-                            <div><b>Expenses:</b> 900 £</div>
-                        </div>
-                    </React.Fragment>
-                } footer={
-                    <div className={classes.FooterLabel}>
-                        <Link to='/statements'>
-                            Details<i className={['fa', 'fa-angle-right'].join(' ')} />
-                        </Link>
-                    </div>
-                } />
-
-                <div className={classes.CardInfoContainer}>
-                    <Card mini={true} type={CardType.Primary} body={
-                        <Link to='/expenses'>
-                            <i className={['fa', 'fa-plus-circle', classes.Fa].join(' ')} />
-                            <div>Add Expenses</div>
-                        </Link>
-                    } />
-
-                    <Card mini={true} type={CardType.Accent} body={
-                        <Link to='/categories'>
-                            <i className={['fa', 'fa-plus-circle', classes.Fa].join(' ')} />
-                            <div>Add New Category</div>
-                        </Link>
-                    } />
-
-                    <Card mini={true} type={CardType.Info} body={
-                        <Link to='/statements'>
-                            <i className={['fa', 'fa-balance-scale', classes.Fa].join(' ')} />
-                            <div>Statements</div>
-                        </Link>
-                    } />
-                </div>
-            </React.Fragment>
-        );
-    }
+interface IProps {
+  income: string;
+  userId: string;
+  loadIncome: (id: string) => any;
+  loadExpenses: (id: string) => any;
+  expenses: any[];
 }
 
-export default Dashboard;
+class Dashboard extends Component<IProps, {}> {
+  public componentDidMount() {
+    const { userId } = this.props;
+    this.props.loadIncome(userId);
+    this.props.loadExpenses(userId);
+  }
+  public render() {
+    const { income, expenses } = this.props;
+    let expensesTotAmount = 0;
+    if (expenses.length > 0) {
+      expensesTotAmount = expenses.reduce((sum: number, expense: any) => {
+        return sum + parseFloat(expense.amount);
+      }, 0);
+    }
+
+    return (
+      <React.Fragment>
+        <div className={classes.Period}>
+          Period:
+          <span className={classes.Date}>27-09-2018</span>
+          <span> to </span>
+          <span className={classes.Date}>27-10-2018</span>
+        </div>
+
+        <Card
+          title="Balance"
+          body={
+            <React.Fragment>
+              <div className={classes.SpendingCircle}>
+                <span className={classes.Balance}>
+                  {parseFloat(income) - expensesTotAmount} £
+                </span>
+              </div>
+              <div className={classes.Details}>
+                <div>
+                  <b>Income: </b>
+                  {income} £
+                </div>
+                <div>
+                  <b>Expenses:</b> {expensesTotAmount} £
+                </div>
+              </div>
+            </React.Fragment>
+          }
+          footer={
+            <div className={classes.FooterLabel}>
+              <Link to="/statements">
+                Details
+                <i className={['fa', 'fa-angle-right'].join(' ')} />
+              </Link>
+            </div>
+          }
+        />
+
+        <div className={classes.CardInfoContainer}>
+          <Card
+            mini={true}
+            type={CardType.Primary}
+            body={
+              <Link to="/expenses">
+                <i className={['fa', 'fa-plus-circle', classes.Fa].join(' ')} />
+                <div>Add Expenses</div>
+              </Link>
+            }
+          />
+
+          <Card
+            mini={true}
+            type={CardType.Accent}
+            body={
+              <Link to="/categories">
+                <i className={['fa', 'fa-plus-circle', classes.Fa].join(' ')} />
+                <div>Add New Category</div>
+              </Link>
+            }
+          />
+
+          <Card
+            mini={true}
+            type={CardType.Info}
+            body={
+              <Link to="/statements">
+                <i
+                  className={['fa', 'fa-balance-scale', classes.Fa].join(' ')}
+                />
+                <div>Statements</div>
+              </Link>
+            }
+          />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+const mapStateToProps = (state: any) => ({
+  userId: state.auth.localId,
+  income: state.admin.income,
+  expenses: state.expenses.expenses,
+});
+
+const mapDispatchToProps = (dispatch: any) => ({
+  loadIncome: (id: string) => dispatch(loadIncomeStart(id)),
+  loadExpenses: (id: string) => dispatch(loadExpensesStart(id)),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Dashboard);
